6b3b6b26e4a72a85e51c7ab08a36e9222fcae9c3
Pon ArunKumar Ramalingam <ponarunkumar@gmail.com>
2018-01-22 08:19:34 -0500

    Updated the log and Address check

--- contracts/Splitter1.sol
+++ contracts/Splitter1.sol
@@ -5,7 +5,7 @@ contract Splitter1 {
     mapping(address => uint) public balances;
     
     event LogContractOwner(address _owner);
-    event LogSenderReceivers(address _sender, address _receiver1, address _receiver2);
+    event LogSenderReceivers(address _sender, address _receiver1, address _receiver2, uint _shareAmount, uint _changeAmount);
     
     function Splitter1() public payable {
         owner = msg.sender;
@@ -13,6 +13,7 @@ contract Splitter1 {
     }
     
     function share(address _receiver1, address _receiver2) public payable returns(bool success) {
+	require(_receiver1 !=0 && _receiver2 !=0);
         require(msg.value>0);
         uint _shareAmount = msg.value/2;  
         uint _changeAmount = msg.value%2;
@@ -21,7 +22,7 @@ contract Splitter1 {
         if(_changeAmount>0) {
                     balances[msg.sender] += _changeAmount;
         }
-        LogSenderReceivers(msg.sender, _receiver1, _receiver2);
+        LogSenderReceivers(msg.sender, _receiver1, _receiver2, _shareAmount, _changeAmount);
         return true;
     }
     
